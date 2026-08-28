45b17f9bc05a07c84bba7332946f99b3115a1fba
Jordy Jimbo <jimbojordy383@gmail.com>
2026-08-27 21:09:32 -0500

    chore: initial project setup with Spring Boot 4.1.1 and PostgreSQL

---
+++ src/test/java/com/jordyjimbo/sistema_citas/SistemaCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordyjimbo.sistema_citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
